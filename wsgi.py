8edc680057aabc3a869e2dd162bd568aa2c4a370
flovahgreen <flovah.green@gmail.com>
2026-04-08 09:29:21 +0900

    Delete wsgi.py

--- wsgi.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-
-# 프로젝트 경로 추가 (PythonAnywhere에서 사용)
-path = os.path.dirname(os.path.abspath(__file__))
-if path not in sys.path:
-    sys.path.insert(0, path)
-
-from server import app as application
